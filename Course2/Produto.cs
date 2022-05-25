487026ee0fc8d10722859e62959303b404202f75
Bruno <bruno.jpinto@outlook.com>
2022-05-25 16:00:38 -0300

    first commit

---
+++ Course2/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace Course2
+{
+    class Produto
+    {
+        //Nome de atributo sempre começa com letra maiúscula
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            //CultureInfo.InvariantCulture serve para usar o ponto como separador de decimais
+            return Nome + ", $" + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $" +
+                ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
